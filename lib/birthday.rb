74af3f79f19f0c144b3d458eb82981bd2552a79e
Merkeresseria Coleman <kttsmllc@gmail.com>
2019-11-18 17:20:53 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,7 +3,7 @@
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
-requiere'pry'
+require'pry'
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kids_name, age|
